1c7f7c3eaa9c85ec10375e07eb5ce1ca832b204c
Famak2003 <Famaki310@gmail.com>
2023-10-24 22:47:47 +0300

    updated

---
+++ src/components/carousel/GalleryCarousel/GalleryCarousel.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import TeamImg from "./TeamImg";
+import { useEffect } from "react";
+
+import BUTTON from "./../../../assets/forward.png";
+import Card from "../../common/Card";
+
+export default function GalleryCarousel({ carouselData, type = "" }) {
+  const [slideNum, setSlideNum] = useState(0);
+  const [translatePosition, setTranslatePosition] = useState(0);
+  const [carouselSliderWidth, setCarouselSliderWidth] = useState(
+    carouselData?.length * 100,
+  );
+  const [navigationButtonArr, setNavigationButtonArr] = useState([]);
+
+  // PREV FUNCTION
+  const prevBtn = () => {
+    if (slideNum === 0) {
+      return setSlideNum(carouselData?.length - 1);
+    }
+    return setSlideNum((prev) => (prev -= 1));
+  };
+
+  // NEXT FUNCTION
+  const nextBtn = () => {
+    if (slideNum === carouselData?.length - 1) {
+      return setSlideNum(0);
+    }
+    return setSlideNum((prev) => (prev += 1));
+  };
+
+  useEffect(() => {
+    setTranslatePosition(slideNum * (100 / carouselData?.length));
+  }, [slideNum, carouselData?.length]);
+
+  useEffect(() => {
+    setCarouselSliderWidth(carouselData?.length * 100);
+    setNavigationButtonArr(
+      Array.from({ length: carouselData?.length }, (_, i) => i),
+    );
+  }, [carouselData?.length]);
+
+  return (
+    <div className=" relative w-full overflow-hidden ">
+      {/* SLIDER */}
+      <ul
+        style={{
+          width: `${carouselSliderWidth}%`,
+          transform: `translateX(-${translatePosition}%)`,
+        }}
+        className=" flex h-fit duration-300 xs:gap-[.5rem] "
+      >
+        {carouselData.map((obj, idx) => {
+          return (
+            <div key={idx} className="w-full">
+              {type === "team" && (
+                <li className=" flex w-full items-center justify-start gap-[.5rem] smobile:gap-[1rem] mobile:gap-[1.5rem] sm:gap-[2rem] md:gap-[2.5rem] lg:gap-[3rem] xl:gap-[4rem]">
+                  {obj.map((item, idx) => {
+                    return (
+                      <TeamImg
+                        key={idx}
+                        img={item.img}
+                        title={item.title}
+                        name={item.name}
+                        experience={item.experience}
+                        idx={obj.length % (idx + 1)}
+                      />
+                    );
+                  })}
+                </li>
+              )}
+              {type === "card" && (
+                <ul className="flex h-full w-full justify-center gap-[4%] py-[1rem] lg:gap-[6%]">
+                  {obj.map((item, index) => {
+                    return (
+                      <Card
+                        key={index}
+                        header={item.header}
+                        img={item.image}
+                        type={"universityCard"}
+                      >
+                        {item.content}
+                      </Card>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+          );
+        })}
+      </ul>
+      {/* PAGINATION */}
+      <div className="mx-auto flex w-fit gap-x-3 py-4  ">
+        {navigationButtonArr?.map((item, id) => {
+          return (
+            <span
+              key={id}
+              onClick={() => setSlideNum(item)}
+              className={`h-2 ${
+                slideNum === item
+                  ? "bg-gray-600 xs:w-[3rem] mobile:w-[3rem] md:h-[.5rem] md:w-[6rem]"
+                  : "w-14 bg-gray-300"
+              }  h-[.3rem] rounded-full bg-[#CDCDCE] duration-300 xs:w-[.7rem] mobile:w-[1rem] md:h-[.5rem] md:w-[1.5rem]`}
+            ></span>
+          );
+        })}
+      </div>
+
+      {/* NAVIGATION BUTTON*/}
+      <div className="absolute bottom-[10%] right-0 z-20 flex w-fit gap-x-5">
+        <button
+          onClick={() => prevBtn()}
+          className=" flex h-3 w-3 cursor-pointer items-center justify-center rounded-full bg-white shadow-[0_1.2863757610321045px_2.572751522064209px_0_rgba(180,180,180,0.25)] mobile:h-4 mobile:w-4 lmobile:h-6 lmobile:w-6 md:h-10 md:w-10"
+        >
+          <img className="rotate-180 scale-50" src={BUTTON} alt="back" />
+        </button>
+        <button
+          onClick={() => nextBtn()}
+          className="flex h-3 w-3 cursor-pointer items-center justify-center rounded-full bg-white shadow-[0_2.572751522064209px_5.145503044128418px_0_rgba(180,180,180,0.25)] mobile:h-4 mobile:w-4 lmobile:h-6 lmobile:w-6 md:h-10 md:w-10"
+        >
+          <img className="scale-50" src={BUTTON} alt="back" />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/homeComponents/OurSpeakers/Speaker.jsx
+++ src/pages/home/homeComponents/OurSpeakers/Speaker.jsx
@@ -1,50 +1,61 @@
 import React from "react";
-import CarouselSpeakers from "../../../../components/CarouselComp/CarouselSpeakers";
+import GalleryCarousel from "./../../../../components/carousel/GalleryCarousel/GalleryCarousel";
 
-import speaker1 from "../../../../assets/speaker1.png";
-import design from "../../../../assets/design.png";
-import register from "../../../../assets/register.png";
-import cto from "../../../../assets/cto.png";
-import chantel from "../../../../assets/chantel.png";
+import TEAMIMG1 from "../../../../assets/Olusola Somuyiwa(CEO and CTO).jpg";
+import TEAMIMG2 from "../../../../assets/Folahan Kilanko(Head of Marketing).png";
+import TEAMIMG3 from "../../../../assets/Prof Idowu Olayinka.jpg";
+import TEAMIMG4 from "../../../../assets/Isa AbdulMajeed(Head of Design).jpg";
+import TEAMIMG5 from "../../../../assets/Chantel Onyebuchi(COO).jpg";
 
-import "./speaker.css";
+// import "./speaker.css";
+
+export const team = [
+  [
+    {
+      img: TEAMIMG1,
+      title: "CEO AND CTO",
+      name: "Olusola Somuyiwa",
+      experience:
+        "4+ years of experience in education technology, and enterpreneurship",
+    },
+    {
+      img: TEAMIMG2,
+      title: "HEAD OF MARKETING",
+      name: "Folahan Kilanko",
+      experience:
+        "4+ years in education sector social media marketing and brand positioning.",
+    },
+    {
+      img: TEAMIMG3,
+      title: "ADVISOR",
+      name: "Prof.idowu Olayinka",
+      experience: "Former Vice Chancellor, university of ibadan",
+    },
+    {
+      img: TEAMIMG4,
+      title: "HEAD OF DESIGN",
+      name: "AbdulMajeed Isa",
+      experience:
+        "5 years of experience in education sector design and user experience",
+    },
+    {
+      img: TEAMIMG5,
+      title: "COO AND COFOUNDER",
+      name: "Chantel Onyebuchi",
+      experience:
+        "4+ years of experience in operations managements in the education sector",
+    },
+  ],
+];
 
 function Speaker() {
-  const carouselItems = [
-    <div className="speakers" key={1}>
-      <div className="speakersItem">
-        <img src={speaker1} alt="" />
-        <h2>CEO</h2>
-      </div>
-      <div className="speakersItem">
-        <img src={design} alt="" />
-        <h2>Head Design</h2>
-      </div>
-      <div className="speakersItem">
-        <img src={register} alt="" />
-        <h2>Jamb Register</h2>
-      </div>
-      <div className="speakersItem">
-        <img src={cto} alt="" />
-        <h2>CTO</h2>
-      </div>
-      <div className="speakersItem">
-        <img src={chantel} alt="" />
-        <h2>Chantel</h2>
-      </div>
-    </div>,
-  ];
-
   return (
-    <div className="testimonials">
-        <div className="backgroundText speakText">
-          <h1>The Speakers</h1>
-        </div>
-      <h2>The Speakers</h2>
-      <div className="speakCont">
-      <CarouselSpeakers items={carouselItems} />
-      </div>
-    </div>
+    <section className=" generalPadding team-back mobile:py[2rem] relative flex h-fit flex-col items-center bg-[#F8FBFF] xs:gap-[1rem] xs:py-[3rem] mobile:gap-[3rem] lmobile:py-[5rem] md:gap-[5rem]">
+      <h1 className=" responsiveHeader text-center font-bold"> The Speaker</h1>
+
+      {/* slider */}
+      <GalleryCarousel carouselData={team} type={"team"} />
+    </section>
   );
 }
 
